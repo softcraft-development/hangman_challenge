2e47562dd02db3f1fdd24212f7e4f2a553213060
Craig Walker <github@softcraft.ca>
2015-12-07 15:01:02 -0700

    Game model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207214955) do
+ActiveRecord::Schema.define(version: 20151207220032) do
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "word_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "games", ["word_id"], name: "index_games_on_word_id"
 
   create_table "words", force: :cascade do |t|
     t.string   "name"
