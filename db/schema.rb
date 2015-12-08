09691e9db6b04c85135ab964c5cde04cd92f6712
Craig Walker <github@softcraft.ca>
2015-12-08 13:32:52 -0700

    Add status to game

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207220242) do
+ActiveRecord::Schema.define(version: 20151208202655) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
   add_index "games", ["word_id"], name: "index_games_on_word_id"
