54d4710e803ff3c1dec1a1f2d8632a9ddda4c6da
Craig Walker <github@softcraft.ca>
2015-12-08 11:15:19 -0700

    determine the game state

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,29 @@ class Game < ActiveRecord::Base
   belongs_to :word
   has_many :guesses
   
+  def state
+    hash = {}
+    hash[:guessed_letters] = guesses.map{|guess| guess.letter.upcase}.sort
+
+    correct_letters = []
+    hash[:positions] = self.word.name.each_char.map do |letter|
+      letter = letter.upcase
+      correct_letters << letter
+      
+      if hash[:guessed_letters].include?(letter)
+        letter
+      else
+        nil
+      end
+    end
+    
+    correct_letters.uniq!
+
+    hash[:misses] = hash[:guessed_letters].reduce(0) do |misses, letter|
+      misses += 1 unless correct_letters.include?(letter)
+      misses
+    end
+    
+    hash
+  end
 end
