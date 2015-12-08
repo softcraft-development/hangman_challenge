d326307323fa44259a55041bec8732779f8390c7
Craig Walker <github@softcraft.ca>
2015-12-08 10:32:28 -0700

    custom jasmine matcher to check against blank values

--- spec/javascripts/helpers/spec_helper.js
+++ spec/javascripts/helpers/spec_helper.js
@@ -7,3 +7,39 @@ beforeEach(module('hangman'));
 beforeEach(inject(function(_$controller_){
   $controller = _$controller_;
 }));
+
+beforeEach(function() {
+  jasmine.addMatchers({
+    toBeBlank: function(util, customEqualityTesters){
+      return {
+        compare: function(actual) {
+          // This lets us also check the contents of a jQuery object
+          if (actual.html) {
+            actual = actual.html();
+          }
+          
+          var result = {};
+          if (actual) {
+            if (typeof(actual) == "string") {
+              result.pass = actual.trim().length == 0;
+            }
+            else {
+              result.pass = false;
+            }
+          }
+          else {
+            // false, null, undefined, 0, etc
+            result.pass = true;
+          }
+          if (result.pass) {
+            result.message = "Expected '" + actual + "' to not be blank";
+          }
+          else {
+            result.message = "Expected '" + actual + "' to be blank";
+          }
+          return result;
+        }
+      }
+    }
+  });
+});
