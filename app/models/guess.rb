784069df3c471e180f825ec20fdf4015fb963361
Craig Walker <github@softcraft.ca>
2015-12-08 14:58:11 -0700

    ensure guesses are always upcased

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,10 @@ class Guess < ActiveRecord::Base
   belongs_to :game
   validates :game, :presence => true
   validates :letter, :presence => true, :uniqueness => {:scope => :game} 
+  
+  before_validation :upcase_letter
+  
+  def upcase_letter
+    self.letter = self.letter.upcase if self.letter
+  end
 end
