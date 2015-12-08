fd64f924520556f7aa409beb693c15f4e26e32bd
Craig Walker <github@softcraft.ca>
2015-12-08 15:48:56 -0700

    extract valid letter range

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,7 +1,8 @@
 class Guess < ActiveRecord::Base
+  RANGE = ("A".."Z")
   belongs_to :game
   validates :game, :presence => true
-  validates :letter, :presence => true, :uniqueness => {:scope => :game} 
+  validates :letter, :presence => true, :uniqueness => {:scope => :game}, :inclusion => { :in => RANGE.to_a } 
   
   before_validation :upcase_letter
   
