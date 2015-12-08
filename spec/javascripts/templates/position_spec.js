8a3d260e135184a151bbb57a02ed29bb4ca50e90
Craig Walker <github@softcraft.ca>
2015-12-08 10:33:29 -0700

    specs for position template

---
+++ spec/javascripts/templates/position_spec.js
@@ -0,0 +1,47 @@
+// inspired by http://thecache.trov.com/unit-testing-angular-views-to-avoid-the-integration-test-scam/
+
+describe("templates", function(){
+  describe("position.html", function(){
+    var template, $rootScope, $scope, $compile, view;
+
+    beforeEach(inject(function($templateCache, _$compile_, _$rootScope_) {  
+      template = $templateCache.get("position.html");
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+      view = angular.element(template);
+    }));
+    
+    describe("when the position is null", function(){
+      beforeEach(function(){
+        $scope.position = null;
+        $compile(view)($scope);
+        $rootScope.$digest();
+      });
+
+      it("sets the contents to empty", function(){
+        expect(view).toBeBlank();
+      });
+      
+      it("adds the blank css class", function(){
+        expect(view).toHaveClass("blank");
+      });
+    });
+
+    describe("when the position is non null", function(){
+      beforeEach(function(){
+        $scope.position = "A Test Value";
+        $compile(view)($scope);
+        $rootScope.$digest();
+      });
+
+      it("sets the contents to the position value", function(){
+        expect(view).toHaveText($scope.position);
+      });
+      
+      it("does not add the blank css class", function(){
+        expect(view).not.toHaveClass("blank");
+      });
+    });
+  });
+});
