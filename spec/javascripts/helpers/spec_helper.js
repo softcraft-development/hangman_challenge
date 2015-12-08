eb77ae47341d39ce045a950e2c0bcf30bc2c8f2a
Craig Walker <github@softcraft.ca>
2015-12-08 11:54:34 -0700

    remove global controller injection
    This isn’t universally applicable to all tests after all

--- spec/javascripts/helpers/spec_helper.js
+++ spec/javascripts/helpers/spec_helper.js
@@ -4,9 +4,6 @@
 // https://code.angularjs.org/1.4.7/docs/guide/unit-testing
 beforeEach(module('hangman'));
 
-beforeEach(inject(function(_$controller_){
-  $controller = _$controller_;
-}));
 
 beforeEach(function() {
   jasmine.addMatchers({
