4159b65be8539441995c621eec2ae39539cdd09e
Craig Walker <github@softcraft.ca>
2015-12-09 14:58:31 -0700

    Fixed commenting on requires

--- spec/javascripts/helpers/spec_helper.js
+++ spec/javascripts/helpers/spec_helper.js
@@ -1,5 +1,5 @@
-#= require angular-mocks
-#= require jasmine-jquery
+//= require angular-mocks
+//= require jasmine-jquery
 
 // https://code.angularjs.org/1.4.7/docs/guide/unit-testing
 beforeEach(module('hangman'));
