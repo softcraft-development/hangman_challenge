6e8766589925b8b89bb3f5cfced13b4cb5e2ff39
Craig Walker <github@softcraft.ca>
2015-12-09 14:46:23 -0700

    Trigger the update event when the number of misses changes

--- app/assets/javascripts/controllers/game.js
+++ app/assets/javascripts/controllers/game.js
@@ -7,6 +7,12 @@ Hangman.Controllers.controller("Game", function($scope, $http, $routeParams){
     debugger;
   });
   
+  $scope.$watch("misses", function(newValue){
+    // Misses happen to match up with frames, so we can pass newValue directly, 
+    // but this is where we'd transform it if necessary.
+    $scope.$emit("update", newValue);
+  });
+  
   $scope.makeGuess = function(letter){
     promise = $http.put("games/" + $routeParams.gameId + "/guesses/" + letter.toUpperCase() + ".json")
     promise.success(function(data){
