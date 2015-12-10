72c415790d368c12f95baf191395d761d4310ecd
Craig Walker <github@softcraft.ca>
2015-12-10 10:49:04 -0700

    update the game status when a guess is made (or destroyed)

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -5,8 +5,15 @@ class Guess < ActiveRecord::Base
   validates :letter, :presence => true, :uniqueness => {:scope => :game}, :inclusion => { :in => RANGE.to_a } 
   
   before_validation :upcase_letter
+  after_save :update_game_status
+  after_destroy :update_game_status
   
   def upcase_letter
     self.letter = self.letter.upcase if self.letter
+  end
+  
+  def update_game_status
+    self.game.determine_status
+    self.game.save!
   end
 end
