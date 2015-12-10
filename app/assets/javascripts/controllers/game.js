acba11c276369a8512a7a0e5e03010b282c7c882
Craig Walker <github@softcraft.ca>
2015-12-10 07:32:34 -0700

    missing semicolon

--- app/assets/javascripts/controllers/game.js
+++ app/assets/javascripts/controllers/game.js
@@ -1,7 +1,7 @@
 Hangman.Controllers.controller("Game", function($scope, $http, $routeParams){
   promise = $http.get("games/" + $routeParams.gameId + ".json")
   promise.success(function(data) {
-    _.extend($scope, data)
+    _.extend($scope, data);
   });
   promise.error(function(){
     debugger;
