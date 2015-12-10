a386d9a9e88190b90432ba4f319303c39a971bf0
Craig Walker <github@softcraft.ca>
2015-12-10 12:21:01 -0700

    test for new game links in home template

--- spec/javascripts/templates/home_html_spec.js
+++ spec/javascripts/templates/home_html_spec.js
@@ -1,6 +1,10 @@
 describe("templates", function(){
   describe("home.html", function(){
-    var template, $rootScope, $scope, $compile, view;
+    var template, $rootScope, $scope, $compile, view, newGame;
+    var render = function(){
+      $compile(view)($scope);
+      $rootScope.$digest();
+    }
 
     beforeEach(inject(function($templateCache, _$compile_, _$rootScope_) {  
       template = $templateCache.get("home.html");
@@ -8,6 +12,8 @@ describe("templates", function(){
       $rootScope = _$rootScope_;
       $scope = $rootScope.$new();
       view = angular.element(template);
+      newGame = jasmine.createSpy("newGame");
+      $scope.$root.newGame = newGame;
     }));
     
     beforeEach(function(){
@@ -15,13 +21,32 @@ describe("templates", function(){
         {id: 3},
         {id: 5},
       ];
-      $compile(view)($scope);
-      $rootScope.$digest();
+      render();
     });
 
     it("adds a link for each open game", function(){
       _.each($scope.openGames, function(game){
         expect(view).toContainElement("a[href='#/game/" + game.id + "']")
+      });
+    });
+    
+    describe("when the new game button in the title is clicked", function(){
+      beforeEach(function(){
+        view.find(".title button.new-game").click();
+      });
+      
+      it("triggers a new game", function(){
+        expect(newGame).toHaveBeenCalled();
+      });
+    });
+
+    describe("when the new game link in the game section is clicked", function(){
+      beforeEach(function(){
+        view.find(".games-section a.new-game").click();
+      });
+      
+      it("triggers a new game", function(){
+        expect(newGame).toHaveBeenCalled();
       });
     });
   });
